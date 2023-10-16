59a975df8e55156d2db4b26d9fa0002dfd3fcc39
Derek Mahar <derek.mahar@protonmail.com>
2023-10-16 17:34:06 -0400

    Add Vim tab settings, buffer key mappings, dark background.

--- vim.nix
+++ vim.nix
@@ -6,6 +6,24 @@
       plugins = with pkgs.vimPlugins; [
         fzf-vim
       ];
+      settings = {
+        expandtab = true;
+        shiftwidth = 4;
+        #softtabstop = 4;
+        tabstop = 4;
+      };
+      extraConfig = ''
+        " Key mappings
+        nnoremap gb :buffers<cr>:buffer<space>
+        nnoremap gd :bd<cr>
+        nnoremap gn :bn<cr>
+        nnoremap gp :bp<cr>
+
+        " Settings
+        color evening
+        set background=dark
+        set softtabstop=4
+      '';
     };
   };
 }
