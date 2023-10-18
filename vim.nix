d3f83d01c864070b0c345b7ed0598a59f14367b5
Derek Mahar <derek.mahar@protonmail.com>
2023-10-18 15:54:57 -0400

    Add Vim key mappings for window and buffer split creation.

--- vim.nix
+++ vim.nix
@@ -14,10 +14,24 @@
       };
       extraConfig = ''
         " Key mappings
+
+        " Buffer navigation
         nnoremap gb :buffers<cr>:buffer<space>
         nnoremap gd :bd<cr>
         nnoremap gn :bn<cr>
         nnoremap gp :bp<cr>
+
+        " Window splits
+        nmap <leader>sw<left>  :topleft vnew<CR>
+        nmap <leader>sw<right> :botright vnew<CR>
+        nmap <leader>sw<up>    :topleft new<CR>
+        nmap <leader>sw<down>  :botright new<CR>
+
+        " Buffer splits
+        nmap <leader>s<left>   :leftabove vnew<CR>
+        nmap <leader>s<right>  :rightbelow vnew<CR>
+        nmap <leader>s<up>     :leftabove new<CR>
+        nmap <leader>s<down>   :rightbelow new<CR>
 
         " Settings
         color evening
