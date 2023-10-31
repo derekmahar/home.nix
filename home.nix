b3c3a203ee32c17e7017bea2d9602260dffed2df
Derek Mahar <derek.mahar@protonmail.com>
2023-10-31 17:56:56 +0000

    Move home.username and home.homeDirectory to user.nix.

--- home.nix
+++ home.nix
@@ -1,13 +1,9 @@
 { config, pkgs, ... }:
 {
   imports = [
+    ./user.nix
     ./vim.nix
   ];
-
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "ubuntu";
-  home.homeDirectory = "/home/ubuntu";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
---
+++ user.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "derek";
+  #home.username = builtins.getEnv "USER"; 
+  home.homeDirectory = "/home/derek";
+  #home.homeDirectory = builtins.getEnv "HOME";
+}
