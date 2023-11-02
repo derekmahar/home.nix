cd05e1437d6920c12f4b65abcae00d1ec0706f5c
Derek Mahar <derek.mahar@proton.me>
2023-11-02 10:05:17 -0400

    Reconfigure tmux based on advice from :checkhealth in nvim.

--- tmux.nix
+++ tmux.nix
@@ -3,9 +3,13 @@
   programs = {
     tmux = {
       enable = true;
+      escapeTime= 10;
       extraConfig = ''
-        set-option -g default-command "\$\{SHELL\}"
+        set-option -g default-command "''${SHELL}"
+        set-option -sa terminal-features ",xterm-256color:RGB"
       '';
+      mouse = true;
+      terminal = "screen-256color";
     };
   };
 }
