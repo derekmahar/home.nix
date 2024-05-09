e58be661d6affa77126970b4c98d6bb52dc64c7a
Derek Mahar <derek.mahar@proton.me>
2024-05-09 14:01:24 -0400

    Explicitly load keys id_rsa and id_ed25519, but ignore any that are missing.

--- keychain.nix
+++ keychain.nix
@@ -4,6 +4,8 @@
     keychain = {
       enable = true;
       enableBashIntegration = true;
+      extraFlags = ["--ignore-missing" "--quiet"];
+      keys = ["id_rsa" "id_ed25519"];
     };
   };
 }
