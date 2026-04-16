71025b197f9b727bbbd6dab8591a85f569c69dee
Derek Mahar <derek.mahar@protonmail.com>
2026-04-16 13:34:38 -0400

    Revert "User "ubuntu" instead of "derek"."
    This reverts commit 3378b1839885f7fbb6a123d66b1303afea211ad3.

--- user.nix
+++ user.nix
@@ -1,8 +1,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "ubuntu";
+  home.username = "derek";
   #home.username = builtins.getEnv "USER"; 
-  home.homeDirectory = "/home/ubuntu";
+  home.homeDirectory = "/home/derek";
   #home.homeDirectory = builtins.getEnv "HOME";
 }
