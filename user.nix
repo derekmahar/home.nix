593bb1eba54ebfbe2582ae7f8e3c77187967bf54
Derek Mahar <derek.mahar@protonmail.com>
2026-06-13 13:58:29 -0400

    User "ubuntu" instead of "derek".

--- user.nix
+++ user.nix
@@ -1,8 +1,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "derek";
+  home.username = "ubuntu";
   #home.username = builtins.getEnv "USER"; 
-  home.homeDirectory = "/home/derek";
+  home.homeDirectory = "/home/ubuntu";
   #home.homeDirectory = builtins.getEnv "HOME";
 }
